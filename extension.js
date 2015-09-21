21479000a26bc6275f8cc25818111198149a6004
Paul Tagliamonte <tag@pault.ag>
2015-09-21 11:32:02 -0400

    toggle

--- extension.js
+++ extension.js
@@ -24,12 +24,7 @@ const MountMenuItem = new Lang.Class({
 
         this.action = action
 
-        // let ejectIcon = new St.Icon({
-        //     icon_name: 'video-display-symbolic',
-        //     style_class: 'popup-menu-icon'
-        // });
-        let ejectButton = new St.Button({/*child: ejectIcon*/});
-        // ejectButton.connect('clicked', Lang.bind(this, this._eject));
+        let ejectButton = new St.Button({});
         this.actor.add(ejectButton);
         this.actor.visible = true;
     },
@@ -45,6 +40,56 @@ const MountMenuItem = new Lang.Class({
         });
     }
 });
+
+
+const APIToggle = new Lang.Class({
+    Name: "APIToggle.APIToggle",
+    Extends: PopupMenu.PopupSwitchMenuItem,
+
+    _init: function(name, api, statusEndpoint, onEndpoint, offEndpoint) {
+        this.parent(name, false);
+        this.api = api;
+        this.statusEndpoint = statusEndpoint;
+        this.onEndpoint = onEndpoint;
+        this.offEndpoint = offEndpoint;
+    },
+    destroy: function() {
+        this.parent();
+    },
+
+    updateStatus: function() {
+        this.api.get("mute", function(err, data) {
+            global.log(data);
+            if (err !== null) {
+                return;
+            }
+            this.setToggleState(data == '1');
+        });
+    },
+
+    enable: function() {
+        this.connect("toggled", Lang.bind(this, this._toggle));
+        this.parent();
+    },
+
+    disable: function() {
+        this.parent();
+    },
+
+    _toggle: function(item, state) {
+        /*
+        if (state) {
+            endpoint = self.onEndpoint;
+        } else {
+            endpoint = self.offEndpoint;
+        }
+
+        this.api.get(endpoint, function(err, data) {
+        });
+        */
+    },
+});
+
 
 const DriveMenu = new Lang.Class({
     Name: 'DriveMenu.DriveMenu',
@@ -64,15 +109,21 @@ const DriveMenu = new Lang.Class({
         this.actor.add_child(hbox);
 
         this._mounts = {
-            "power": [
-                {"path": "power/on",  "name": "Power On"},
-                {"path": "power/off", "name": "Power Off"},
-            ],
+            // "power": [
+            //     {"path": "power/on",  "name": "Power On"},
+            //     {"path": "power/off", "name": "Power Off"},
+            // ],
             "mute": [
                 {"path": "mute/on",   "name": "Mute On"},
                 {"path": "mute/off",  "name": "Mute Off"},
             ],
         }
+
+        // this.powerToggle = new APIToggle("Power", "power", "power/on", "power/off");
+        this.powerToggle = new APIToggle("Mute", this.api, "mute", "mute/on", "mute/off");
+        this.powerToggle.updateStatus();
+        this.menu.addMenuItem(this.powerToggle);
+        // this.powerToggle.connect("toggled", Lang.bind(this, _toggle));
 
         for (classification in this._mounts) {
             value = this._mounts[classification];
